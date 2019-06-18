9b09f658187dd9800094b449d008caa03364af23
Esther Hwang <estshwang@gmail.com>
2019-06-18 00:54:58 -0500

    added pseudo code to router.js

---
+++ public/assets/js/app.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+//reference
+/* <div class="card shadow-sm">
+  <h5 class="card-header">PlaceHolderTitle <a href="#" class="btn btn-primary float-right">Save Article</a></h5>
+  <div class="card-body">
+    <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa ea illum deserunt doloremque corporis vero molestias sint dignissimos voluptatum accusamus!</p> */
+
+//save article button
+$("#saveArticle").on("click", function() {
+
+})
+//delete saved article button
+$("#").on("click", function() {
+    
+})
+//comment submission button
+$("#").on("click", function() {
+    
+})
+//scrape article button
+$("#").on("click", function() {
+    
+})
+//clear articles button
+$("#").on("click", function() {
+    
+})
+//if no articles are saved then show suggestion to scrape 
+if () {
+
+} else {
+
+}
--- routes/router.js
+++ routes/router.js
@@ -1,34 +1,61 @@
 var db = require("../models/index");
 const express = require("express");
+const axios = require("axios");
+const mongojs = require("mongojs");
+var cheerio = require("cheerio");
 const app = express();
+
+//database config
+var databaseURL= "scraper";
+var collections = ["scrapedData"];
+
+//connect mongojs config to db variable
+var db = mongojs(databaseURL, collections);
+db.on("error", function(error) {
+    console.log("Database Error:", error);
+});
 
 module.exports = function(app) {
     //routing to home page
     app.get("/", function(req, res) {
-        res.render("index", {title: "MongoScraper Home"});
+        res.render("index", {res, style: "style", title: "MongoScraper Home"});
     });
 
     //scrape articles
-    // app.get("/scrape", function(req, res){
+    app.get("/scrape", function(req, res){
+        axios.get("").then(function(response) {
+            var $ = cheerio.load(response.data);
 
-    // })
+            $(".title").each(function(i, element) {
+                var title = $(element).children("a").text();
+                var link = $(element).children("a").attr("href");
+
+                if (title && link) {
+                    db.scraped
+                }
+;            });
+        });
+    });
 
     //routing to saved articles page
-    // app.get("/savedArticles", function(req, res){
-
-    // });
+    app.get("/savedArticles", function(req, res){
+        res.render("articles", {res, style: "article", title: "Saved Articles"});
+    });
 
     //save article
     // app.post("/saved", function(req, res){
 
     // });
 
+    //clear article from saved page
+    //app.post("/clearArticle", function(req, res){});
+
     //posting a comment
     // app.post("/comments", function(req, res){
 
     // });
 
-    //editing comment
+    //deleting comment
     // app.post("/comments/:id", function(req, res){
 
     // });
