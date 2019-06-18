de7ab17fd85390a6adb12d2d34ce8efc5b599276
Esther Hwang <estshwang@gmail.com>
2019-06-18 18:10:05 -0500

    fixed structure of model files

--- routes/router.js
+++ routes/router.js
@@ -19,52 +19,7 @@ db.on("error", function(error) {
 });
 
 module.exports = function(app) {
-    //routing to home page
-    app.get("/", function(req, res) {
-        res.render("index", {res, style: "style", title: "MongoScraper Home"});
-    });
-
-    //scrape articles
-    app.get("/scrape", function(req, res){
-        
-        axios.get("https://www.nytimes.com/topic/subject/rock-climbing").then(function(response) {
-            //capturing html into cheerio and saving as variable
-            var $ = cheerio.load(response.data);//change
-
-            //an empty array to save data that we'll scrape
-            
-
-                $("article").each(function(i, element){
-                    var results = {};
-                    
-                    var title = $(this).find("h2").text();
-                    title = title.split(" ").join(" ").split("\n").join(" ").trim();
-
-                    var link = $(this).find("a").attr("href");
-                    var image = $(this).find("img").attr("src");
-                    var summary = $(this).find(".summary").text();
-                    //r&d how to refine search to just class summary
-                    
-                    results.push({
-                        title: title,
-                        link: link,
-                        image: image,
-                        summary: summary
-                    })
-
-                    db.Article.create(results).then(function(dbArticle){
-                        console.log(dbArticle);
-                    }).catch(function(err){
-                        console.log(err);
-                        
-                    })
-                });
-            //    console.log(results);
-               res.json(results);
-            // res.redirect("/");
-            })
-            
-    });
+   
 
     //routing to saved articles page
     app.get("/savedArticles", function(req, res){
