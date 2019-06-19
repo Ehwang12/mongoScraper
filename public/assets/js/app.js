e163ff22b96e9361b70e51bb249226785e672443
Esther Hwang <estshwang@gmail.com>
2019-06-19 00:03:23 -0500

    added modal functionality to comment button

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,9 +10,16 @@ $("#clearOne").on("click", function() {
     console.log("delete article") 
 })
 
-// comment submission button
-$("#comt-btn").on("click", function() {
-    console.log("Comment on article")
+// comment modal button
+$('#comment-mod').on('show.bs.modal', function (event) {
+  console.log("this is working")
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text("Add a Comment")
+  modal.find('.modal-body input').val()
 })
 
 //scrape article button
